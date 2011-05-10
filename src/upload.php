d0a6239e95f3e29c773cef7326002285118786e1
lds <lds2012@gmail.com>
2011-05-10 23:32:59 +0800

    import

---
+++ src/upload.php
@@ -0,0 +1,16 @@
+<?php
+var_dump($_FILES);
+
+?>
+<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title> 饭否 | 发照片</title>
+ <link rel="stylesheet" href="http://static.fanfou.com/css/m.css?v=50997e52" type="text/css" media="screen" charset="utf-8" /></head>
+<body>
+<h1><a href="/home"><img src="http://static.fanfou.com/i/fanfou.gif" alt="饭否" /></a></h1>
+<h2>发照片</h2>
+<p class="n">照片格式错误</p>
+<p class="n">发送成功</p>
+</body>
+</html>
